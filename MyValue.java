b3a51578044cb690dacb3cdbb27e7aaa33bc61a9
LerishaDsouza <41681429+LerishaDsouza@users.noreply.github.com>
2020-06-22 03:44:33 -0700

    Add files via upload

---
+++ MyValue.java
@@ -0,0 +1,19 @@
+class MyValue{
+	public static void main(String[] args)
+	{
+
+	System.out.println("Enter the number");
+
+	int v = new java.util.Scanner(System.in).nextInt();
+	
+	int i, f = 1;
+
+	for(i=v;i >= 1; i=i-1 )
+	{
+	f = f*i;
+	}
+
+	System.out.println("The factorial of a number is:"+f);
+	
+	}
+}
