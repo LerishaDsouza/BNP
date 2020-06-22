6302cae6559f3bb22af611fa3ff0ef08a0ca5d80
LerishaDsouza <41681429+LerishaDsouza@users.noreply.github.com>
2020-06-22 04:09:17 -0700

    Add files via upload

---
+++ Seasons.java
@@ -0,0 +1,36 @@
+class Seasons{
+	
+	public static void main(String[] args){
+
+		System.out.println("Enter the month number");
+
+		int var = new java.util.Scanner(System.in).nextInt();
+
+		switch(var){
+
+		case 12:
+		case 1:
+		case 2: System.out.println("Winter");
+		break;
+
+		case 3:
+		case 4:
+		case 5: System.out.println("Spring");
+		break;
+
+		case 6:
+		case 7:
+		case 8: System.out.println("Summer");
+		break;
+
+		case 9:
+		case 10:
+		case 11: System.out.println("Autumn");
+		break;
+
+		default: System.out.println("No Such Season Exists");
+	
+	}
+
+	}
+}
