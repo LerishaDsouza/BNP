77960f8f386953733500fc4a2b4a0d90361533ec
LerishaDsouza <41681429+LerishaDsouza@users.noreply.github.com>
2020-06-22 04:12:44 -0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+class Fibonacci{
+
+	public static void main(String[] args){
+
+		System.out.println("Enter the number of termms");
+	
+		int n = new java.util.Scanner(System.in).nextInt();
+	
+		int a = 0, b = 1;
+
+		System.out.print(b + " ");
+	
+		for(int i = 1; i <= n; i++){
+	
+		int z = a + b;
+
+		System.out.print(z + " ");
+
+		a = b;
+
+		b = z;
+
+	
+		}
+	}
+}
